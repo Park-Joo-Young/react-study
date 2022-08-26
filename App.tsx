fa2930e29d3e3240615589b9bd4e68de54edfa94
Park-Joo-Young <shety4611@gmail.com>
2022-08-26 13:17:09 +0900

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { createContext, useContext } from 'react';
+import './style.css';
+const themeDefault = { border: '10px solid green' };
+const themeContext = createContext(themeDefault);
+
+export default function App() {
+  const theme = useContext(themeContext);
+  return (
+    <themeContext.Provider value={{ border: '10px solid blue' }}>
+      <div className="root" style={theme}>
+        <h1>React - Context API</h1>
+        <Sub1 />
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function Sub1() {
+  const theme = useContext(themeContext);
+  return (
+    <themeContext.Provider value={{ border: '10px solid red' }}>
+      <div style={theme}>
+        <h1>Sub1</h1>
+        <Sub2 />
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function Sub2() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3 />
+    </div>
+  );
+}
+
+function Sub3() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
+    </div>
+  );
+}
